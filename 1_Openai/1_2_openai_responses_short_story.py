db5776a8228bdcededeedada85f8d60ea3b99751
akash <akash.borkar2@gmail.com>
2026-02-15 11:23:20 +0530

    new commit

--- 1_Openai/1_2_openai_responses_short_story.py
+++ 1_Openai/1_2_openai_responses_short_story.py
@@ -5,20 +5,13 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 client = OpenAI(
-    base_url="https://kodekey.ai.kodekloud.com/v1",
-    api_key=os.getenv("KODEKLOUD_API_KEY")
+    api_key=os.getenv("OPENAI_API_KEY")
 )
 
-messages = [
-    {
-        "role": "user",
-        "content": "In a single sentence, why should someone choose KodeKloud over other platforms to learn DevOps?"
-    }
-]
 
-response = client.chat.completions.create(
-    model="openai/gpt-4o-2024-11-20",
-    messages=messages
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input="Write a one-sentence bedtime story about a unicorn."
 )
 
-print(response.choices[0].message.content)
+print(response.output_text)
